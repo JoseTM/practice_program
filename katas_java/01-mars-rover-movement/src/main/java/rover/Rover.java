f347c22ed5159b3e166dc682dd513736f97ce4d1
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2020-09-09 00:27:10 +0100

    adición de test de movmientos añado test para el moviento vuelta al mundo

--- katas_java/01-mars-rover-movement/src/main/java/rover/Rover.java
+++ katas_java/01-mars-rover-movement/src/main/java/rover/Rover.java
@@ -1,5 +1,9 @@
 package rover;
 
+/**
+ * supondremos que el mundo mide 5 * 5
+ */
+
 public class Rover {
   private int posX;
   private int posY;
@@ -37,64 +41,70 @@ public class Rover {
   
   private void computeMovementBackward(String movimiento) {
   if (orientation == Orientation.NORTH) {
-    --this.posY;
+    this.posY = --this.posY;
     this.orientation = Orientation.SOUTH;
     return;
   }
   if (orientation == Orientation.SOUTH) {
-    ++this.posY;
+    this.posY = ++this.posY;
     this.orientation = Orientation.NORTH;
     return;        
   }
   if (orientation == Orientation.WEST) {
-    --this.posX;
+    this.posX = --this.posX;
     this.orientation = Orientation.EAST;
     return;        
   }
   if (orientation == Orientation.EAST) {
-    ++this.posX;
+    this.posX = ++this.posX;
     this.orientation = Orientation.WEST;
     return;        
   }
 }
 
-  private void computeMovementForward(String movimiento) {
+private void computeMovementForward(String movimiento){
+    internalComputeMovementForward(movimiento);
+   if(this.posX == 6){
+      this.posX = 0;
+    }
+  if(this.posY == 6){
+    this.posY = 0;
+  }
+}
+
+private void internalComputeMovementForward(String movimiento) {
   if (orientation == Orientation.NORTH) {
-    ++this.posY;
+    this.posY = ++this.posY;
     return;
   }
   if (orientation == Orientation.SOUTH) {
-    --this.posY;
+    this.posY = --this.posY;
     return;        
   }
   if (orientation == Orientation.WEST) {
-    ++this.posY;
+    this.posX = ++this.posX;
     return;        
   }
   if (orientation == Orientation.EAST) {
-    --this.posY;
+    this.posX = --this.posX;
     return;        
   }
 }
 
   private void computeMovementLeft(String movimiento) {
   if (orientation == Orientation.NORTH) {
-    --this.posX;
     this.orientation = Orientation.EAST;
     return;
   }
   if (orientation == Orientation.SOUTH) {
-    ++this.posX;
     this.orientation = Orientation.WEST;
     return;        
   }
   if (orientation == Orientation.WEST) {
-    --this.posY;
     this.orientation = Orientation.NORTH;
     return;        
   }
   if (orientation == Orientation.EAST) {
-    ++this.posY;
     this.orientation = Orientation.SOUTH;
     return;        
   }
@@ -102,22 +112,18 @@ public class Rover {
 
   private void computeMovementRight(String movimiento) {
   if (orientation == Orientation.NORTH) {
-    ++this.posX;
     this.orientation = Orientation.WEST;
     return;
   }
   if (orientation == Orientation.SOUTH) {
-    --this.posX;
     this.orientation = Orientation.EAST;
     return;        
   }
   if (orientation == Orientation.WEST) {
-    ++this.posY;
     this.orientation = Orientation.SOUTH;
     return;        
   }
   if (orientation == Orientation.EAST) {
-    --this.posY;
     this.orientation = Orientation.NORTH;
     return;        
   }
--- katas_java/01-mars-rover-movement/src/test/java/RoverTestMovement.java
+++ katas_java/01-mars-rover-movement/src/test/java/RoverTestMovement.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,10 +10,39 @@ import rover.Rover;
 public class RoverTestMovement{
 
   @Test
+  public void moveX(){
+    Rover rover = new Rover(0,0,Orientation.WEST);
+    rover.receive(Movement.FORWARD);
+    assertTrue(new Rover(0,1,Orientation.WEST).equals(rover));
+  }
+
+  @Test
+  public void moveY(){
+    Rover rover = new Rover(0,0,Orientation.WEST);
+    rover.receive(Movement.LEFT+Movement.FORWARD);
+    assertTrue(new Rover(1,0, Orientation.NORTH).equals(rover));
+  }
+
+  @Test
   public void rover_move_around(){
     Rover rover = new Rover(3,5,Orientation.WEST);
     rover.receive(Movement.LEFT+Movement.LEFT+Movement.LEFT+Movement.LEFT);
-    assertEquals(new Rover(3,5,Orientation.WEST), rover); 
+    assertTrue(new Rover(3,5,Orientation.WEST).equals(rover));
+  }
+
+  @Test
+  public void rover_world_horizontal_travel(){
+    Rover rover = new Rover(0,0,Orientation.WEST);
+    rover.receive(Movement.FORWARD+Movement.FORWARD+Movement.FORWARD+Movement.FORWARD+Movement.FORWARD+Movement.FORWARD);
+    assertTrue(new Rover(0,0,Orientation.WEST).equals(rover));
+  }
+
+
+  @Test
+  public void rover_world_vertical_travel(){
+    Rover rover = new Rover(1,3,Orientation.SOUTH);
+    rover.receive(Movement.FORWARD+Movement.FORWARD+Movement.FORWARD+Movement.FORWARD+Movement.FORWARD+Movement.FORWARD);
+    assertTrue( new Rover(1,3,Orientation.SOUTH).equals(rover));
   }
   
 }
